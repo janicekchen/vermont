67f7939a3682ecd5d77298479051aef8fcf15b24
janicekchen <janicekchen@gmail.com>
2021-01-01 07:05:36 -0500

    update mp4

--- tree.js
+++ tree.js
@@ -3,27 +3,40 @@ var scroller = scrollama();
 function init() {
     scroller
         .setup({
-            step: '#scrollTree article .step',
+            step: '#scrollTree article .stepTree',
             progress: true,
             offset: 0,
             debug: true,
-
         })
         .onStepProgress(handleStepProgress);
 }
+var vid = document.getElementById('treevid');
+vid.onloadedmetadata = function() {
+    console.log('hey')
+  };
+
+
 
 function handleStepProgress(response) {
     var vid = document.getElementById('treevid');
 
-    const fallTime = 3.859;
-   console.log(vid.currentTime);
+    var duration = 0;
+
+    vid.onloadedmetadata = function() {
+        console.log('hey')
+        duration = this.duration;
+      };
+      
+    const singleTree = 3.615; //single tree fall time
+    
     var progress = response.progress;
+    console.log(duration)
     if (progress > 0 && progress < 1) {
-        vid.pause();
-        vid.currentTime = fallTime * progress;
+        vid.currentTime = vid.duration * progress;
+        console.log(vid.currentTime)
     } 
     else if (progress == 1) {
-        vid.play();
+
     }
     //var progress = el.attr('progress');
     //var index = el.attr('index');
